c3608c632819b72080069b99ed87bf601b35493b
Serguei Sokol <sokol at insa-toulouse.fr>
2014-11-25 14:40:11 +0100

    added RcppKalman::expm() to benchmark; matrix sizes are now 5, 50 and 500 to get reasonable bench times; increased threshold in diff() between higham and ward expm();

--- test/test_expm.R
+++ test/test_expm.R
@@ -1,15 +1,17 @@
 # test rex2arma() on a real word example, a function calculating
 # matrix exponential: expm.Higham08()
 
-require(expm)
 require(rbenchmark)
+require(RcppKalman)
+require(expm)
 #source("../rex2arma.inc.R"); source("../rex2arma.R")
-require(Rcpp)
+require(Rcpp, lib="~/.R/library")
+
 
 # The original R code for expm(A) by a method of Higham is accessible
 # from the package expm, function expm.Higham08
 
-# The original R code was modified in 3 places before submission to rex2arma():
+# The original R code was modified in 4 places before submission to rex2arma():
 # 1. "I <- if (is(A, "Matrix")) Diagonal(n) else diag(n)"
 #    was replaced by "I <- diag(n)" as two differnet types (here sparse and dense
 #    matrices) cannot be assigned to the same variable (here I)
@@ -31,7 +33,7 @@ source("expm.higham.R") # create expm.higham() function
 # translate it in C++ expm_cpp() function
 A=diag(2) # just examples of two input parameters
 balancing=TRUE
-cppp_code=rex2arma(expm.higham, fname="expm_cpp", exec=0, rebuild=TRUE, copy=FALSE)
+rex_code=rex2arma(expm.higham, fname="expm_cpp", exec=1, rebuild=TRUE, copy=FALSE)
 
 # make tests and benchmarks
 # Let do it on three matrix sizes: small (s, n=10), medium (m, n=100) and
@@ -40,41 +42,51 @@ cppp_code=rex2arma(expm.higham, fname="expm_cpp", exec=0, rebuild=TRUE, copy=FAL
 set.seed(7) # make the tests reproducible
 
 # small
-n=10
+n=5
 As=matrix(rnorm(n*n), n)
-stopifnot(diff(range(expm.higham(As, balancing=TRUE)-expm_cpp(As, balancing=TRUE))) < 1.e-14)
-stopifnot(diff(range(expm.higham(As, balancing=FALSE)-expm_cpp(As, balancing=FALSE))) < 1.e-14)
+cat(sprintf("small matrix (%d x %d) benchmark\n", n, n))
 print(benchmark(
    expm.higham(As, balancing=TRUE),
    expm.higham(As, balancing=FALSE),
    expm_cpp(As, balancing=TRUE),
    expm_cpp(As, balancing=FALSE),
+   RcppKalman::expm(As),
+   replications=500,
    order="relative"
 )[,1:4])
+stopifnot(diff(range(expm.higham(As, balancing=TRUE)-expm_cpp(As, balancing=TRUE))) < 1.e-14)
+stopifnot(diff(range(expm.higham(As, balancing=FALSE)-expm_cpp(As, balancing=FALSE))) < 1.e-14)
+stopifnot(diff(range(expm.higham(As, balancing=FALSE)-RcppKalman::expm(As))) < 1.e-13)
 
 # medium
-n=100
+n=50
 As=matrix(rnorm(n*n), n)
-stopifnot(diff(range(expm.higham(As, balancing=TRUE)-expm_cpp(As, balancing=TRUE))) < 1.e-14)
-stopifnot(diff(range(expm.higham(As, balancing=FALSE)-expm_cpp(As, balancing=FALSE))) < 1.e-14)
+cat(sprintf("medium matrix (%d x %d) benchmark\n", n, n))
 print(benchmark(
    expm.higham(As, balancing=TRUE),
    expm.higham(As, balancing=FALSE),
    expm_cpp(As, balancing=TRUE),
    expm_cpp(As, balancing=FALSE),
+   RcppKalman::expm(As),
    order="relative"
 )[,1:4])
-
-# large
-n=800
-As=matrix(rnorm(n*n), n)
 stopifnot(diff(range(expm.higham(As, balancing=TRUE)-expm_cpp(As, balancing=TRUE))) < 1.e-14)
 stopifnot(diff(range(expm.higham(As, balancing=FALSE)-expm_cpp(As, balancing=FALSE))) < 1.e-14)
+stopifnot(diff(range(expm.higham(As, balancing=FALSE)-RcppKalman::expm(As))) < 1.e-9)
+
+# large
+n=500
+As=matrix(rnorm(n*n), n)
+cat(sprintf("large matrix (%d x %d) benchmark\n", n, n))
 print(benchmark(
    expm.higham(As, balancing=TRUE),
    expm.higham(As, balancing=FALSE),
    expm_cpp(As, balancing=TRUE),
    expm_cpp(As, balancing=FALSE),
-   order="relative",
-   replications=5
+   RcppKalman::expm(As),
+   replications=5,
+   order="relative"
 )[,1:4])
+stopifnot(diff(range(expm.higham(As, balancing=TRUE)-expm_cpp(As, balancing=TRUE))) < 1.e-14)
+stopifnot(diff(range(expm.higham(As, balancing=FALSE)-expm_cpp(As, balancing=FALSE))) < 1.e-14)
+stopifnot(diff(range(expm.higham(As, balancing=FALSE)-RcppKalman::expm(As))) < 2.e-4)
